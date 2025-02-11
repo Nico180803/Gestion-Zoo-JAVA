a25488689608d1ed2d7cadec644fabc3f0a0c498
Nico180803 <nicolascharpentier1808@gmail.com>
2025-02-11 17:23:11 +0100

    feat: class enclos

--- src/Zoo/Enclos.java
+++ src/Zoo/Enclos.java
@@ -1,14 +1,36 @@
 package Zoo;
 
+import Animal.Animal;
+import Humain.Soigneur;
+
+import java.util.ArrayList;
+
 public class Enclos {
     private String nom;
     private int capacite;
     private int sale;
+    ArrayList<Animal> animaux;
 
     public Enclos(String nom, int capacite, int sale) {
         this.nom = nom;
         this.capacite = capacite;
         this.sale = sale;
+        this.animaux = new ArrayList<Animal>();
+    }
+
+    public void ajouterAnimal(Animal animal) {
+        if (capacite == 0) {
+            System.out.println("L'Enclos est Plein !");
+        }else{
+            this.animaux.add(animal);
+            this.capacite--;
+            System.out.println(animal.getNom()+" est mis dans l'enclos "+this.getNom()+"\nIl reste "+this.getCapacite()+" place dans l'enclo "+this.getNom());
+        }
+    }
+
+    public void nettoyage(Soigneur soigneur) {
+        System.out.println(soigneur.getNom()+" nettoie l'enclos "+this.getNom());
+        this.sale = 0;
     }
 
     public String getNom() {
