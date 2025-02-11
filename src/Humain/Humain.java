ca2ada5d07342862fdb2d33645be1806bed0c7fa
Theo Enfedaque <t.enfedaque@lprs.fr>
2025-02-11 17:02:58 +0100

    reparation de l'extreme

--- src/Humain/Humain.java
+++ src/Humain/Humain.java
@@ -1,4 +1,40 @@
 package Humain;
 
 public class Humain {
+    private String nom;
+    private String prenom;
+    private int age;
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 }
