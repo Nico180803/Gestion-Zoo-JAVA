4f039a500f4ed2fc29542a37b6f769633ecaffb9
Mateo LAPETRE <m.lapetre-sarthou@lprs.fr>
2025-03-04 10:21:32 +0100

    refactor: ajout de spectacle

--- src/Humain/Visiteur.java
+++ src/Humain/Visiteur.java
@@ -1,8 +1,20 @@
 package Humain;
 
+import Animal.*;
+
 public final class Visiteur extends Humain{
 
     public Visiteur(String nom, String prenom, int age) {
         super(nom, prenom, age, "Visiteur");
+    }
+
+    public void reagis(Animal animal){
+        if (animal instanceof Lion)
+            System.out.println ("Les visiteurs applaudisse");
+        else if (animal instanceof Oiseau) {
+            System.out.println ("Les visiteurs sont stupéfaits");
+        }else {
+            System.out.println("Les visiteur s'exclament");
+        }
     }
 }
--- src/Zoo/Spectacle.java
+++ src/Zoo/Spectacle.java
@@ -10,19 +10,38 @@ public class Spectacle {
 
     private String nom;
     private int nbPlace;
+    private int soigneurGerant;
     private ArrayList<Animal> animals;
     private ArrayList<Soigneur> soigneurs;
     private ArrayList<Visiteur> visiteurs;
-    public Spectacle(String nom, int nbPlace) {
+    public Spectacle(String nom, int nbPlace, int soigneurGerant) {
         this.nom = nom;
         this.nbPlace = nbPlace;
+        this.soigneurGerant = soigneurGerant;
+
         this.animals = new ArrayList<Animal>();
         this.soigneurs = new ArrayList<Soigneur>();
         this.visiteurs = new ArrayList<Visiteur>();
     }
-
-    public void prestation(){
-
+    public void ajouterAnimal(Animal a) {
+        animals.add(a);
+        System.out.println(a.getNom() + " a bien été ajouté");
+    }
+    public void ajouterSoigneur(Soigneur s) {
+        soigneurs.add(s);
+        System.out.println(s.getNom() + " a bien été ajouté");
+    }
+    public void ajouterVisiteur(Visiteur v) {
+        visiteurs.add(v);
+        System.out.println(v.getNom()+ " a bien été ajouté");
+    }
+    public void prestation(String nomTour, int animal){
+        System.out.println("Gérant : "+soigneurs.get(this.soigneurGerant));
+        System.out.println("Bonjour à tous, aujourd'hui je vais vous presentez le tour intituler : "+nomTour+" réaliser par : "+animals.get(animal));
+        System.out.println(animals.get(animal)+" réalise son tour");
+        System.out.println();
+        animals.get(animal).bruit();
+        visiteurs.get(0).reagis(animals.get(animal));
     }
 
     public String getNom() {
